f4d8f98892ce879134ef1590b59d6aade5edc123
Inbydev <owner@inby.dev>
2024-05-24 18:38:03 -0400

    updated config of astro for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    prefetch: true
+    prefetch: true,
+    compressHTML: false,
+    build: {
+        inlineStylesheets: 'never'
+    }
 })
