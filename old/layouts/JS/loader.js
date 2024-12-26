92b72ddf42fc967ba33ca9ece22373b006a88d53
inbydev <owner@inby.dev>
2024-12-25 22:13:23 -0300

    Removed old files from main branch

--- old/layouts/JS/loader.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    const loader = document.querySelector('.icon-load');
-    loader.style.opacity = 0;
-
-    setTimeout(function() {
-        loader.style.display = 'none';
-    }, 300);
-};
