c4fbf470daa0431df4cd21ec7a5977883a6e72e4
inbydev <owner@inby.dev>
2025-07-14 02:20:08 -0400

    Updated astro config so now it has sitemap + uses the Images components for images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-  build: {
-    assets: "assets",
-  },
+    site: 'https://inby.dev',
+    prefetch: true,
+
+    build: {
+        assets: "assets",
+    },
+
+    responsiveImages: true,
+
+    integrations: [
+        sitemap({
+            changefreq: 'monthly',
+            priority: 1.0,
+            lastmod: new Date('2025-05-14'),
+        }),
+    ],
 });
